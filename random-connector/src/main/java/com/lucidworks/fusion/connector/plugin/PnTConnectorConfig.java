6b960f69186f3f0dcff9598d36438d98e05263d7
acesar <cesar.vera-bernal@lucidworks.com>
2019-03-18 13:08:58 -0500

    fix 4.1.0 integration.

--- random-connector/src/main/java/com/lucidworks/fusion/connector/plugin/PnTConnectorConfig.java
+++ random-connector/src/main/java/com/lucidworks/fusion/connector/plugin/PnTConnectorConfig.java
@@ -4,43 +4,38 @@ import com.lucidworks.fusion.connector.plugin.api.config.ConnectorConfig;
 import com.lucidworks.fusion.connector.plugin.api.config.ConnectorPluginProperties;
 import com.lucidworks.fusion.schema.SchemaAnnotations.Property;
 import com.lucidworks.fusion.schema.SchemaAnnotations.RootSchema;
+import com.lucidworks.fusion.schema.SchemaAnnotations.StringSchema;
 
 @RootSchema(
     name = "redhat.pnt.connector",
-    title = "PnT Connector",
+    title = "PnT Connector", // TODO What does Pnt mean?
     description = "A connector that retrieves documents from alfresco instance",
     category = "CMIS"
 )
 public interface PnTConnectorConfig extends ConnectorConfig<PnTConnectorConfig.Properties> {
 
-  @Property(
-      title = "Properties",
-      required = true
-  )
+  @Property(title = "Properties", required = true)
   Properties properties();
 
-  /**
-   * Connector specific settings
-   */
   interface Properties extends ConnectorPluginProperties {
 
-    @Property(
-            title = "Username",
-            description = "User who is accessing the alfresco instance"
-    )
+    @Property(title = "Username", description = "User who is accessing the alfresco instance")
     String username();
 
-    @Property(
-            title = "Password"
-    )
+    @Property(title = "Password")
+    @StringSchema(encrypted = true)
     String password();
 
     @Property(
-            title = "Start folder",
-            description = "Start location of the crawl (location must start with / and not end with /)"
+        title = "Alfesco Url",
+        description = "")
+    String url();
+
+    @Property(
+        title = "Start folder",
+        description = "Start location of the crawl (location must start with / and not end with /)"
     )
+    @StringSchema(defaultValue = "/")
     String startFolder();
-
   }
-
 }
--- random-connector/src/main/java/com/lucidworks/fusion/connector/plugin/PnTConnectorFetcher.java
+++ random-connector/src/main/java/com/lucidworks/fusion/connector/plugin/PnTConnectorFetcher.java
@@ -9,6 +9,7 @@ import com.lucidworks.fusion.connector.plugin.api.fetcher.type.content.MessageHe
 import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.opencmis.client.api.Property;
 import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
@@ -20,78 +21,76 @@ import java.util.stream.IntStream;
 
 public class PnTConnectorFetcher implements ContentFetcher {
 
-  private final Logger logger;
+  private static final Logger logger = LogManager.getLogger(PnTConnectorFetcher.class);
+
   private final PnTConnectorConfig pntConnectorConfig;
 
   private String username;
   private String password;
   private String startFolder;
+  private String url;
+
   private Session session;
   private PnTContentCrawler pnTContentCrawler;
   private List<String> documentIdList;
 
   @Inject
-  public PnTConnectorFetcher(
-      Logger logger,
-      PnTConnectorConfig pntConnectorConfig
-  ) {
-    this.logger = logger;
+  public PnTConnectorFetcher(PnTConnectorConfig pntConnectorConfig) {
     this.pntConnectorConfig = pntConnectorConfig;
   }
 
   @Override
   public StartResult start(StartContext startContext) {
-
     username = pntConnectorConfig.properties().username();
     password = pntConnectorConfig.properties().password();
+    url = pntConnectorConfig.properties().url();
     startFolder = pntConnectorConfig.properties().startFolder();
 
-    session = PnTConnectorClient.getConnectorClient(username,password).getSession();
+    session = PnTConnectorClient.getConnectorClient(username, password, url).getSession();
 
     pnTContentCrawler = new PnTContentCrawler(startFolder);
 
     documentIdList = pnTContentCrawler.getAllDocuments(session);
 
-    //List<Phase> phaseList = new ArrayList<>();
-    //phaseList.add(Phase.builder("test-phase1").build());
-    //phaseList.add(Phase.builder("test-phase2").build());
-
-    return new StartResult();//StartResult.builder().withPhases(phaseList).build();
+    return startContext.newResult();
   }
 
   @Override
   public PreFetchResult preFetch(PreFetchContext preFetchContext) {
 
-    IntStream.range(0, documentIdList.size()).asLongStream().forEach(i -> {
-      logger.info("Emitting candidate -> number {}", i);
-      Map<String, Object> data = Collections.singletonMap("number", (int)i);
-      preFetchContext.emitCandidate(MessageHelper.candidate(String.valueOf(i), Collections.emptyMap(), data).build());
-    });
+    IntStream.range(0, documentIdList.size())
+        .asLongStream()
+        .forEach(
+            i -> {
+              logger.info("Emitting candidate -> number {}", i);
+              Map<String, Object> data = Collections.singletonMap("number", (int) i);
+              preFetchContext.emitCandidate(
+                  MessageHelper.candidate(String.valueOf(i), Collections.emptyMap(), data).build());
+            });
     return preFetchContext.newResult();
   }
 
   @Override
   public FetchResult fetch(FetchContext fetchContext) {
 
-    Map<String,Object> contentMap = new HashMap<>();
+    Map<String, Object> contentMap = new HashMap<>();
 
     FetchInput input = fetchContext.getFetchInput();
     logger.info("Received FetchInput -> {}", input);
 
     long num = getNumber(input);
 
-    Document document = (Document)session.getObject(documentIdList.get((int)num));
+    Document document = (Document) session.getObject(documentIdList.get((int) num));
     logger.info("Emitting Document -> number {}", num);
 
-    for(Property p : document.getProperties()){
-      contentMap.put(p.getId()+"_s",p.getValueAsString());
+    for (Property p : document.getProperties()) {
+      contentMap.put(p.getId() + "_s", p.getValueAsString());
     }
 
     fetchContext.emitDocument(contentMap);
 
     return fetchContext.newResult();
   }
-
 
   private long getNumber(FetchInput input) {
     Object num = input.getMetadata().get("number");
@@ -103,5 +102,4 @@ public class PnTConnectorFetcher implements ContentFetcher {
       throw new RuntimeException(String.format("Invalid value for number: %s", num));
     }
   }
-
 }
